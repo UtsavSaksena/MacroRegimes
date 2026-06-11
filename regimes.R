433434d72ed30391b9377aa0ed2bfdf23a9b7b33
Utsav Saksena <usaksena95@gmail.com>
2026-06-11 15:11:14 +0530

    Modify dataset to explicitly add COVID dummies to help with actual macro regime detection.

--- regimes.R
+++ regimes.R
@@ -1,6 +1,8 @@
 source("dataPrep.R")
 library(factoextra)
 library(DescTools)
+library(plotly)
+library(scatterplot3d)
 
 #############################################
 ############  MACHINE LEARNING ##############
@@ -18,38 +20,36 @@ scale_dat <- scale(dat)
 
 scale_dat_nCovid <- scale_dat[!(rownames(scale_dat) %in% c("2020 Q1","2021 Q1","2021 Q2")),]
 
+###
+mah <- mahalanobis(scale_dat, colMeans(scale_dat), cov(scale_dat))
+plot(mah, type="l")
+abline(h = qchisq(0.999, df = ncol(scale_dat)), col="red", lty=2)
+
+# Any observation beyond the chi-sq threshold is a multivariate outlier
+which(mah > qchisq(0.999, df = ncol(scale_dat)))
+
+
 ### K-means
 set.seed(300)
 
 ## Witn COVID
-fviz_nbclust(scale_dat, kmeans, method = "wss")
+fviz_nbclust(scale_dat, kmeans, method = "wss", k.max = 10)
 fviz_nbclust(scale_dat, kmeans, method = "silhouette")
 fviz_nbclust(scale_dat, kmeans, method = "gap_stat")
 
-km_res <- kmeans(scale_dat, centers = 5, nstart = 50)
+km_res <- kmeans(scale_dat, centers = 3, nstart = 50)
 
 fviz_cluster(km_res, scale_dat) 
 
 library(dbscan)
 
 kNNdistplot(scale_dat, k = 5)
-abline(h = 1.5, col = "red", lty = 2) 
+abline(h = 1.8, col = "red", lty = 2) 
 
-db_result <- dbscan(scale_dat, eps = 1.5, minPts = 5) 
+db_result <- dbscan(scale_dat, eps = 1.8, minPts = 5) 
 db_result |> summary()
 
 hdb_result <- hdbscan(scale_dat, minPts = 5)
-
-viz_dat <- dat
-
-viz_dat$HDBCluster <- as.factor(hdb_result$cluster)
-
-summary(viz_dat$HDBCluster)
-
-
-as.data.frame(viz_dat) |>
-    group_by(HDBCluster) |>
-    summarise(across(c(gdp_sa_g, cpi_iw_g1_q, tbill), mean))
 
 ## Moving to GMM
 library(mclust)
@@ -58,9 +58,25 @@ fit_all <- Mclust(scale_dat)
 summary(fit_all)
 
 plot(fit_all, what = "BIC")
+plot(fit_all, what = "classification")
+
+icl_gmm <- mclustICL(scale_dat)
+plot(icl_gmm)
 
 dat_viz <- as.data.frame(dat)
 dat_viz$gmm_cluster <- fit_all$classification
+
+plot_ly(dat_viz, x = ~gdp_sa_g, y = ~cpi_iw_g1_q, z = ~tbill,
+        color = ~gmm_cluster,
+        mode = "markers+text",
+        text = rownames(dat_viz)
+        ) |>
+    add_markers() |>
+    layout(scene = list(xaxis = list(title = 'GDP'),
+                        yaxis = list(title = 'Inflation'),
+                        zaxis = list(title = 'T-Bill Yield'))
+           )
+
 dat_viz$gmm_prob <- apply(fit_all$z, 1, max)
 
 dat_viz |>
@@ -69,35 +85,6 @@ dat_viz |>
 
 table(dat_viz$gmm_cluster)
 head(round(fit_all$z, 3), 100)
-
-#### 
-## library(mclust)
-
-## X <- as.data.frame( scale(dat_nCovid[, c("gdp_sa_g", "cpi_iw_q_g", "tbill")]))
-
-## bic_grid <- mclustBIC(X, G = 2:4)
-## plot(bic_grid)
-
-## fit_best <- Mclust(X, x = bic_grid)
-## summary(fit_best)
-
-## dat_nCovid$gmm_cluster <- fit_best$classification
-
-## aggregate(dat_nCovid[, c("gdp_sa_g", "cpi_iw_q_g", "tbill")],
-##           by = list(cluster = dat_nCovid$gmm_cluster),
-##           mean)
-
-
-
-## ###
-## fit3 <- Mclust(X, G = 3)
-## summary(fit3)
-## fit3$parameters$pro
-## fit3$parameters$mean
-## table(fit3$classification)
-
-## X$GMM_cluster <- fit3$classification
-
 
 ## HMM
 library(depmixS4)
@@ -127,20 +114,8 @@ viz_dat |>
 
 ###########################################################################
 
-library(plotly)
-library(scatterplot3d)
-
-fig <- plot_ly(viz_dat, x = ~gdp_sa_g, y = ~cpi_iw_g1_q, z = ~tbill,
-               color = ~Cluster) |>
-    add_markers() |>
-    layout(scene = list(xaxis = list(title = 'GDP'),
-                     yaxis = list(title = 'Inflation'),
-                     zaxis = list(title = 'T-Bill Yield'))
-           )
-
-
-scatterplot3d(x = viz_dat$gdp_sa_g,
-              y = viz_dat$cpi_iw_g1_q,
-              z = viz_dat$tbill, pch = 10,
+scatterplot3d(x = dat_viz$gdp_sa_g,
+              y = dat_viz$cpi_iw_g1_q,
+              z = dat_viz$tbill, pch = 10,
               highlight.3d = TRUE)
 
